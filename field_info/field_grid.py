2f31f3497ba4f3f13fc475c76d2406e15e40bfa7
Jason Bono <jbono@fnal.gov>
2019-09-02 13:51:59 -0500

    first result with grid method done

--- field_info/field_grid.py
+++ field_info/field_grid.py
@@ -1,56 +1,53 @@
 import numpy as np
 import pandas as pd
+import math
+import matplotlib.pyplot as plt
 
 def cartesian_field(b,x,y):
     """
-    call with: b an np array of multipole moments of the field in ppm
-    x,y a position in space in mm
-    
-    returns the total magnetic field at the point x,y
-    """
-    
-    x = float(x)
-    y = float(y)
+        call with: b an np array of multipole moments of the field in ppm
+        x,y a position in space in mm
+        
+        returns the total magnetic field at the point x,y
+        """
     
     #Define R in terms of x and y
     r = np.sqrt(x**2 + y**2)
     R = r/45.0
     #Define theta
-    if (y==0):
-        theta = 0
-    elif ((x==0) and (y>0)):
-        theta = np.pi/2
-    elif ((x==0) and (y<0)):
-        theta = -np.pi/2
-    else:
-        theta = np.arctan(y/x)
-
+    theta = np.arctan2(y,x)
+    
     #Add the contribution to the field from the dipole
+    field = 0
     field = b[0]
-    #Define the contribution to the field from the odd multipoles
+    
+    #Add the contribution to the field from the multipole moments
     N = np.size(b)
-    #first the odd/normal multipoles
-    for n in range(1,N,2):
-        field += (R**n)*b[n]*np.cos(n*theta)
-    #finally the even/skew multipoles
-    for n in range(2,N,2):
-        field += (R**n)*b[n]*np.sin(n*theta)
-
+    #first the normal multipoles
+    for n in range(1,math.ceil((N+1)/2.0)):
+        field += (R**n)*b[2*n - 1]*np.cos(n*theta)
+    #finally the skew multipoles
+    for n in range(1,math.ceil((N)/2.0)):
+        field += (R**n)*b[2*n]*np.sin(n*theta)
+    
     return field
 
 
-get_field_grid(b):
+
+
+
+def get_field_grid(b):
     """
         Input: the field multipole moments in ppm
         Output: A pandas df with cartesian coordinates the value of the field
     
         Note: The grid is hard-coded to match the grid from the tracker team
-        it goes from -59 mm to 59 mm in both x and y in steps of 1 mm
+        it goes from -59 mm to 59 mm in both x and y in steps of 2 mm
     """
 
     #get the values of x,y, and B for every point on the grid
-    pos = range(-59,60)
-    vals = [(x,y,cartesian_field(a,x,y)) for x in pos for y in pos]
+    pos = range(-59,60,2)
+    vals = [(x,y,cartesian_field(b,x,y)) for x in pos for y in pos]
     vals = np.array(vals)
 
     #now make a pandas df and fill it with the grid values
@@ -59,6 +56,17 @@ get_field_grid(b):
     df['x'] = vals[:,0]
     df['y'] = vals[:,1]
     df['B'] = vals[:,2]
-    df['B'].apply((lambda x: x + cartesian_field(a,0,0)))
+    
     return df
 
+
+
+def plot_field(df,bins=60):
+    fig = plt.figure(figsize=(10, 8))
+    plt.hist2d(df['x'], df['y'], weights=df['B'], bins=bins,cmap='inferno')
+    plt.xlabel('x (mm)')
+    plt.ylabel('y (mm)')
+    cbar = plt.colorbar()
+    cbar.ax.set_ylabel('B Field')
+    plt.close()
+    return fig
